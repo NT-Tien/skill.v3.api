7181d774beca5a2d10f5e1204bb627f5d38dd79c
Tiến Nguyễn Trọng <tiennguyentrong@Tiens-Laptop.local>
2024-03-17 16:32:57 +0700

    test deploy

--- src/modules/file-manager/file.module.ts
+++ src/modules/file-manager/file.module.ts
@@ -1,20 +1,20 @@
-import { Module } from "@nestjs/common";
-import { FileController } from "./file.controller";
-import { FileService } from "./file.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { FileEntity } from "src/entities/file.entity";
-import { ScheduleModule } from "@nestjs/schedule";
-import { ProductEntity } from "src/entities/product.entity";
-import { AuthModule } from "../auth/auth.module";
+// import { Module } from "@nestjs/common";
+// import { FileController } from "./file.controller";
+// import { FileService } from "./file.service";
+// import { TypeOrmModule } from "@nestjs/typeorm";
+// import { FileEntity } from "src/entities/file.entity";
+// import { ScheduleModule } from "@nestjs/schedule";
+// import { ProductEntity } from "src/entities/product.entity";
+// import { AuthModule } from "../auth/auth.module";
 
-@Module({
-    imports: [
-        TypeOrmModule.forFeature([FileEntity]),
-        TypeOrmModule.forFeature([ProductEntity]),
-        ScheduleModule.forRoot(),
-        AuthModule
-    ],
-    providers: [FileService],
-    controllers: [FileController],
-})
-export class FileModule { }
+// @Module({
+//     imports: [
+//         TypeOrmModule.forFeature([FileEntity]),
+//         TypeOrmModule.forFeature([ProductEntity]),
+//         ScheduleModule.forRoot(),
+//         AuthModule
+//     ],
+//     providers: [FileService],
+//     controllers: [FileController],
+// })
+// export class FileModule { }
