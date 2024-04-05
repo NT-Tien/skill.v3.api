597c0f8e41a0cd638cb97af2599354aabeb32b1f
Tiến Nguyễn Trọng <tiennguyentrong@Tiens-Laptop.local>
2024-04-06 02:06:18 +0700

    update validate add record checkin

--- src/modules/ticket/ticket-order-checkin/ticket-order-checkin.service.ts
+++ src/modules/ticket/ticket-order-checkin/ticket-order-checkin.service.ts
@@ -4,24 +4,39 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { TicketOrderCheckinEntity } from "../entities/ticket-order-checkin.entity";
 import { Repository } from "typeorm";
 import { TicketOrderEntity } from "../entities/ticket-order.entity";
+import { TicketOrderItemEntity } from "../entities/ticket-order-item.entity";
 
 @Injectable()
-export class TicketOrderCheckinService implements TicketOrderCheckinServiceInterface{
+export class TicketOrderCheckinService implements TicketOrderCheckinServiceInterface {
     constructor(
         @InjectRepository(TicketOrderCheckinEntity) private ticketOrderCheckinRepository: Repository<TicketOrderCheckinEntity>,
-        @InjectRepository(TicketOrderEntity) private ticketOrderRepository: Repository<TicketOrderEntity>
+        @InjectRepository(TicketOrderEntity) private ticketOrderRepository: Repository<TicketOrderEntity>,
     ) { }
     async addCheckinRecord(idOrder: string, idItem: string): Promise<any> {
         // check order is deleted
-        var order = await this.ticketOrderRepository.findOne({where: {id: idOrder}});
-        console.log(order);
-        
+        var order = await this.ticketOrderRepository
+            .createQueryBuilder("TICKET_ORDER")
+            .leftJoinAndSelect("TICKET_ORDER.items", "TICKET_ORDER_ITEM")
+            .where("TICKET_ORDER.id = :id", { id: idOrder })
+            .leftJoin("TICKET_ORDER_ITEM.ticket", "TICKET")
+            .addSelect(["TICKET.id"])
+            .getOne();
         if (order?.deletedAt !== null) {
             throw new HttpException("Order info not valid", 400);
         }
-        return this.ticketOrderCheckinRepository.save({idOrder, idItem});
+        // get checkin record
+        var record = await this.ticketOrderCheckinRepository.find({ where: { idOrder, idItem } });
+        // find idItem in order
+        var item = order.items.find(item => item.id === idItem);
+        if (item === undefined) {
+            throw new HttpException("Item not found", 400);
+        }
+        if (record.length >= item.quantity) {
+            throw new HttpException("Record is existed", 400);
+        }
+        return this.ticketOrderCheckinRepository.save({ idOrder, idItem });
     }
     getCheckinRecords(idOrder: string): Promise<any> {
-        return this.ticketOrderCheckinRepository.find({where: {idOrder}});
+        return this.ticketOrderCheckinRepository.find({ where: { idOrder } });
     }
 }
